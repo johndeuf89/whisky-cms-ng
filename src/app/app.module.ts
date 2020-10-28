2daea93d38b1284071ea3144671da3ef668684bf
Jonathan Pipard <jonathan.pipard@gmail.com>
2020-10-28 23:15:22 +0100

    mise en place des modules sur angular cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BlogpostComponent } from './blogpost/blogpost.component';
+import { BlogpostListComponent } from './blogpost-list/blogpost-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogpostComponent,
+    BlogpostListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
